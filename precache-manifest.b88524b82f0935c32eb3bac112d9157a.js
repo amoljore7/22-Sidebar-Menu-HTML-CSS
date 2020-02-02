f8570718ff18dcc22d87c5688b64146fc43310b1
Amol Jore <amol.b@orchids.edu.in>
2020-02-02 10:24:10 +0530

    Updates

---
+++ precache-manifest.b88524b82f0935c32eb3bac112d9157a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a9e075cd79176261195dcdd8d1fa3b",
+    "url": "/22-Sidebar-Menu-HTML-CSS/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/22-Sidebar-Menu-HTML-CSS/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "131690c0745b9a760669",
+    "url": "/22-Sidebar-Menu-HTML-CSS/static/js/main.a046be4f.chunk.js"
+  },
+  {
+    "revision": "f70e6d5152a4cdeeca6d",
+    "url": "/22-Sidebar-Menu-HTML-CSS/static/js/runtime~main.6d245b30.js"
+  }
+]);
