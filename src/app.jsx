a99d76d37a71102192819e45043dff5d605712bf
Amol Jore <amol.b@orchids.edu.in>
2020-02-02 11:16:41 +0530

    WIP - Worked on side bar content

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
+import './app.css';
 class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        Hello React
-      </React.Fragment>
+      <div className='sidebar'>
+        <header>My App</header>
+        <ul>
+          <li><a href="#"><i className="fas fa-qrcode"></i>Dashbord</a></li>
+          <li><a href="#"><i className="fas fa-link"></i>Shortcuts</a></li>
+          <li><a href="#"><i className="fas fa-stream"></i>Overview</a></li>
+          <li><a href="#"><i className="fas fa-calendar-week"></i>Event</a></li>
+          <li><a href="#"><i className="far fa-question-circle"></i>About</a></li>
+          <li><a href="#"><i className="fas fa-sliders-h"></i>Service</a></li>
+          <li><a href="#"><i className="fas fa-envelope"></i>Contact</a></li>
+        </ul>
+      </div>
     );
   }
 
